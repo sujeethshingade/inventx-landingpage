264b3b72c361027641b7c26d0ee8fdfb93382050
Sujeeth Shingade <108385080+sujeethshingade@users.noreply.github.com>
2024-06-24 20:11:01 +0530

    Generate out folder

--- next.config.js
+++ next.config.js
@@ -3,12 +3,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // output: 'export',   // Remove the comment to generate the out file
   images: {
+    unoptimized: true,   // Turned off image optimization
     domains: ["localhost","aceternity.com","images.unsplash.com", "media.istockphoto.com"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.sanity.io",
+        hostname: "**",
         port: "",
       },
     ],
