8256c1c8950649500c274b079dc7eaa0e9c4437e
Kishore-A-2005 <iamkishore61@gmail.com>
2024-06-25 22:32:49 +0530

    What we stand for

--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -1,20 +1,27 @@
 import { Menu } from "@/types/menu";
 
 const menuData: Menu[] = [
+  
   {
     id: 1,
-    title: "Persona",
+    title: "Home",
     newTab: false,
     path: "/",
   },
   {
     id: 2,
+    title: "Persona",
+    newTab: false,
+    path: "/",
+  },
+  {
+    id: 3,
     title: "Resources",
     newTab: false,
     path: "/blog",
   },
   {
-    id: 3,
+    id: 4,
     title: "Company",
     newTab: false,
     path: "/about",
