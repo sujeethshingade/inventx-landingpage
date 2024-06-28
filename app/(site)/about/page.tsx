e4bb9c08347c3f38b787017dfb305eb9772f9dcc
khushal <khushal2004a@gmail.com>
2024-06-28 22:44:26 +0530

    added about us

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -4,10 +4,14 @@ import Contact from "@/components/Contact";
 
 export default function about() {
     return <>
+        <div className="flex justify-center items-center mt-20">
+            <div className="md:w-4/5 w-11/12">
+                <About/>
+            </div>
+        </div>
         <div className="pt-20">
-        <StickyScrollRevealDemo/>
-        {/* <Contact />
-        <About/> */}
+            <StickyScrollRevealDemo/>
+            {/* <Contact /> */}
         </div>
     </>
 }
