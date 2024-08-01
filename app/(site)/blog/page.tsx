bd744b7ecd40f62cd7fe7053fe02faebda836de9
khushal <khushal2004a@gmail.com>
2024-08-01 21:09:49 +0530

    changes

--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -12,15 +12,15 @@ const BlogPage = async () => {
   return (
     <>
     {/* <!-- ===== Blog Title Start ===== --> */}
-    <section className="py-30">
+      <section className="py-30">
         <div className="mx-auto max-w-c-1280 px-4 md:px-8 xl:px-0">
-          <h1 className="text-5xl font-bold text-center">All Blogs</h1>
+          <h1 className="text-2xl text-primary font-semibold text-center">BLOGS</h1>
         </div>
       </section>
       {/* <!-- ===== Blog Title End ===== --> */}
       {/* <!-- ===== Blog Grid Start ===== --> */}
-      <section className="py-20 lg:py-25 xl:py-30">
-        <div className="mx-auto mt-15 max-w-c-1280 px-4 md:px-8 xl:mt-0 xl:px-0">
+      <section className="mb-50">
+        <div className="mx-auto max-w-c-1280 px-4 md:px-8 xl:px-0">
           <div className="grid grid-cols-1 gap-7.5 md:grid-cols-2 lg:grid-cols-3 xl:gap-10">
             {BlogData.map((post, key) => (
               <BlogItem key={key} blog={post} />
